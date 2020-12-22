c256967f92cf795f9b48cfa27367df5692cdaa33
Neumann88 <jacky65rus@gmail.com>
2020-12-22 18:32:20 +1100

    add routes,layout,views

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
   <div id="nav">
-    <h1 class="hello">Hello</h1>
-    <router-view />
+    <component :is="layout">
+      <router-view />
+    </component>
   </div>
 </template>
 
+<script>
+import EmptyLayout from "@/layout/EmptyLayout.vue";
+import MainLayout from "@/layout/MainLayout.vue";
+
+export default {
+  computed: {
+    layout() {
+      return (this.$route.meta.layout || "empty") + "-layout";
+    },
+  },
+  components: {
+    EmptyLayout,
+    MainLayout,
+  },
+};
+</script>
 <style lang="scss">
 @import "~materialize-css/dist/css/materialize.min.css";
 @import "styles/index.css";
