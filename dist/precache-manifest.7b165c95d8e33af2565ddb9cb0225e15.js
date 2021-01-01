7b195e5263f80fef82d47a7291c7157d88861bde
Neumann88 <jacky65rus@gmail.com>
2021-01-02 05:05:14 +1100

    deploy

---
+++ dist/precache-manifest.7b165c95d8e33af2565ddb9cb0225e15.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b84c2d07a83de9caff",
+    "url": "/Vue3-CRM/css/app.71198659.css"
+  },
+  {
+    "revision": "a04b3a33896466a9fc59",
+    "url": "/Vue3-CRM/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "2d9aa081575792ca0ec02a85192abfa4",
+    "url": "/Vue3-CRM/index.html"
+  },
+  {
+    "revision": "b8b84c2d07a83de9caff",
+    "url": "/Vue3-CRM/js/app.329ed87f.js"
+  },
+  {
+    "revision": "13ac6a9129799550e422",
+    "url": "/Vue3-CRM/js/chunk-023e9801.a81c90ae.js"
+  },
+  {
+    "revision": "aaef26a9109f67586baf",
+    "url": "/Vue3-CRM/js/chunk-124f1103.9ec6f629.js"
+  },
+  {
+    "revision": "fd0769e96dbad0bcf22c",
+    "url": "/Vue3-CRM/js/chunk-17ed1754.f8e731ae.js"
+  },
+  {
+    "revision": "bdc744645c4256917649",
+    "url": "/Vue3-CRM/js/chunk-2bf6db0f.620b1c21.js"
+  },
+  {
+    "revision": "81fde322f1c95e3666d9",
+    "url": "/Vue3-CRM/js/chunk-2d0c26db.10115a88.js"
+  },
+  {
+    "revision": "276f485bddab79bbd1d5",
+    "url": "/Vue3-CRM/js/chunk-2d0de4e8.df5b45d9.js"
+  },
+  {
+    "revision": "910dbc939c79990de451",
+    "url": "/Vue3-CRM/js/chunk-2d21a565.7729543b.js"
+  },
+  {
+    "revision": "a6459a214c6500e8183a",
+    "url": "/Vue3-CRM/js/chunk-2d221c6f.7ede2a82.js"
+  },
+  {
+    "revision": "cfce77bd3c7d22d45c3c",
+    "url": "/Vue3-CRM/js/chunk-7d63000a.179b60cc.js"
+  },
+  {
+    "revision": "0db0900b20c707b69719",
+    "url": "/Vue3-CRM/js/chunk-a4b08c0e.938d8abf.js"
+  },
+  {
+    "revision": "a04b3a33896466a9fc59",
+    "url": "/Vue3-CRM/js/chunk-vendors.9dadb3db.js"
+  },
+  {
+    "revision": "6a107e0a79ad5a3accdcfd712c9b3fc2",
+    "url": "/Vue3-CRM/manifest.json"
+  }
+]);
